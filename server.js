ed7ce0d3c799111f5e70e008e9b8f626f28580bc
ISAAN_2_5_8 <amir.modarresi.13@gmail.com>
2022-04-13 00:33:31 +0430

    Add credit endpoint

--- server.js
+++ server.js
@@ -1,51 +1,104 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const { body, validationResult } = require('express-validator');
+const bodyParser = require('body-parser')
 
-const fs = require('fs')
-let rawdata = fs.readFileSync('users.json')
-const users = JSON.parse(rawdata)
+const app = express();
+app.use(bodyParser.json())
 
-app.get('/users', (req, res) => {
-    let pageSize = parseInt(req.query.size)
-    if (!pageSize) {
-        pageSize = 10
-    }
+const port = 3000;
 
-    const pageCount = Math.ceil(users.length / pageSize)
+const fs = require("fs");
+let rawdata = fs.readFileSync("users.json");
+const users = JSON.parse(rawdata);
 
-    let page = parseInt(req.query.page)
+const findUserById = (id) =>
+  users.find((user) => user.id == id || user.email === id);
 
-    if (!page) {
-        page = 1
-    }
+app.get("/users", (req, res) => {
+  let pageSize = parseInt(req.query.size);
+  if (!pageSize) {
+    pageSize = 10;
+  }
 
-    if (page > pageCount) {
-        page = pageCount
-    }
+  const pageCount = Math.ceil(users.length / pageSize);
 
-    res.json({
-        "page": page,
-        "pageCount": pageCount,
-        "posts": users.slice(page * pageSize - pageSize, page * pageSize)
+  let page = parseInt(req.query.page);
+
+  if (!page) {
+    page = 1;
+  }
+
+  if (page > pageCount) {
+    page = pageCount;
+  }
+
+  res.json({
+    page: page,
+    pageCount: pageCount,
+    posts: users.slice(page * pageSize - pageSize, page * pageSize),
+  });
+});
+
+app.get("/users/:id", (req, res) => {
+  const user = findUserById(req.params.id);
+
+  if (!user) {
+    return res.status(404).json({
+      error: 404,
+      message: "User not found",
     });
-})
+  }
 
-app.get('/users/:email', (req, res) => {
-    const email = req.params.email
-    const user = users.find((user) => user.email === email)
+  res.json(user);
+});
+
+app.put(
+  "/users/:id/credit",
+  body("amount")
+    .isInt({ gt: 0 })
+    .withMessage("`amount` field must be non-negative number")
+    .notEmpty()
+    .withMessage("`amount` field is required"),
+  body("operation")
+    .notEmpty()
+    .withMessage("`operation` field is required")
+    .isIn(["increase", "decrease"])
+    .withMessage(
+      "`operation` field must be one of these values: [increase, decrease]"
+    ),
+  (req, res) => {
+    const user = findUserById(req.params.id);
 
     if (!user) {
-        res.status(404).json({
-            error: 404,
-            message: "User not found"
-        })
-        return
+      return res.status(404).json({
+        error: 404,
+        message: "User not found",
+      });
     }
 
-    res.json(user)
-})
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const amount = req.body.amount
+    if (req.body.operation === 'decrease') {
+      if (amount > user.credit) {
+        return res.status(422).json({
+          error: 422,
+          message: "User's credit is not enough for the operation",
+        });
+      }
+
+      user.credit -= amount
+    } else {
+      user.credit += amount
+    }
+
+    return res.status(204).send();
+  }
+);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
